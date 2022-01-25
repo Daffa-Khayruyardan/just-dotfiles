9e7c3e86d32d9afc246acceb33c0c945acde4209
H4Ik3n90 <daffakhayru@gmail.com>
2022-01-25 08:54:07 +0700

    update files changed

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # !/bin/sh 
 
+# init color variable
+
+
 # install dependencies
 sudo apt install wget curl git php 
 
@@ -7,7 +10,6 @@ sudo apt install wget curl git php
 clear 
 
 # setup dotfiles logo
-# start logo
 echo -e "${GREEN} ###################################"
-echo -e "${GREEN} #       ${CYAN}Setup Dotfiles${GREEN}      #"
+echo -e "${GREEN} #         ${CYAN}Setup Dotfiles${GREEN}          #"
 echo -e "${GREEN} ###################################"
