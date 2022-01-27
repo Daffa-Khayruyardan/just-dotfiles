54a143090632b06fad722634a212a4a7a15cda7a
H4Ik3n90 <daffakhayru@gmail.com>
2022-01-27 12:07:27 +0700

    update files changed

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -7,6 +7,10 @@ alias gadd="git add"
 alias gcom="git commit"
 alias gswitch="git checkout"
 alias gpush="git push"
+alias gclone="git clone"
 
 # theme alacritty changer
 alias at="alacritty-themes"
+
+# icon ls 
+alias lst="colorls"
